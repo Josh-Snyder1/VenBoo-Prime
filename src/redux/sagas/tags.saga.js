2944218d50d88e4eb08abdf32558471b021af8b0
Josh Snyder <joshrsnyder@gmail.com>
2022-07-26 15:06:30 -0500

    manage tags form view edit add delete working

--- src/redux/sagas/tags.saga.js
+++ src/redux/sagas/tags.saga.js
@@ -12,8 +12,41 @@ function* fetchTags() {
     }
 }
 
+function* addTag(req) {
+    try{
+        yield axios.post(`/api/tags`, req.payload);
+        yield put ({ type: 'FETCH_TAGS'})
+    }
+    catch (err) {
+        console.error('error in add tag post', err);
+    }
+}
+
+function* deleteTag(req) {
+    try{
+        yield axios.delete(`/api/tags/${req.payload.id}`)
+        yield put ({ type: 'FETCH_TAGS'})
+    }
+    catch (err) {
+        console.error('error in delete tag saga', err)
+    }
+}
+
+function* editTag(req) {
+    try{
+        yield axios.put(`/api/tags/${req.payload.id}`,req.payload);
+        yield put ({ type: 'FETCH_TAGS'});
+    }
+    catch (err) {
+        console.error('error in tags saga', err)
+    }
+}
+
 function* tagSaga() {
   yield takeLatest('FETCH_TAGS', fetchTags);
+  yield takeLatest('ADD_TAG', addTag);
+  yield takeLatest('DELETE_TAG', deleteTag);
+  yield takeLatest('EDIT_TAG',  editTag);
 }
 
 export default tagSaga;
