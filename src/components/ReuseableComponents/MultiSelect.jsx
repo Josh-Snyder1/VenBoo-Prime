40171a5ed686183ea0696a550d63c4a4a49afa12
Tarek Azzazi <azzazi.tarek@gmail.com>
2022-07-29 15:18:21 -0500

    conditional rendered tags edit Profile form

--- src/components/ReuseableComponents/MultiSelect.jsx
+++ src/components/ReuseableComponents/MultiSelect.jsx
@@ -1,11 +1,11 @@
-import * as React from 'react';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import ListItemText from '@mui/material/ListItemText';
-import Select from '@mui/material/Select';
-import Checkbox from '@mui/material/Checkbox';
+import * as React from "react";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import ListItemText from "@mui/material/ListItemText";
+import Select from "@mui/material/Select";
+import Checkbox from "@mui/material/Checkbox";
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
@@ -18,51 +18,51 @@ const MenuProps = {
   },
 };
 
-
-
-export default function MultipleSelectCheckmarks({props}) {
+export default function MultipleSelectCheckmarks({ props }) {
+  console.log("Props are >>>>>>>>>", props);
   const [tagName, setTagName] = React.useState([]);
 
   const tagSelection = props.tagSelection;
-let value;
+  let value;
   const handleChange = (event) => {
     const {
       target: { value },
     } = event;
     setTagName(
       // On autofill we get a stringified value.
-      typeof value === 'string' ? value.split(',') : value,
+      typeof value === "string" ? value.split(",") : value
     );
-    tagSelection(value)
+    tagSelection(value);
   };
 
-  let skills = ['communication', 'teamwork']
+  let skills = ["communication", "teamwork"];
 
   // console.log('in multiselect', tagSelection)
 
   return (
-    props.tags.length > 0 &&
-    <div>
-      <FormControl sx={{ m: 1, width: 300 }}>
-        <InputLabel id="demo-multiple-checkbox-label">Tag</InputLabel>
-        <Select
-          labelId="demo-multiple-checkbox-label"
-          id="demo-multiple-checkbox"
-          multiple
-          value={tagName}
-          onChange={handleChange}
-          input={<OutlinedInput label="Tag" />}
-          renderValue={(selected) => selected.join(', ')}
-          MenuProps={MenuProps}
-        >
-          {props.tags.map((name) => (
-            <MenuItem key={name.name} value={name.name}>
-              <Checkbox checked={tagName.indexOf(name.name) > -1} />
-              <ListItemText primary={name.name} />
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-    </div>
+    props.tags.length > 0 && (
+      <div>
+        <FormControl sx={{ m: 1, width: 300 }}>
+          <InputLabel id="demo-multiple-checkbox-label">Tag</InputLabel>
+          <Select
+            labelId="demo-multiple-checkbox-label"
+            id="demo-multiple-checkbox"
+            multiple
+            value={tagName}
+            onChange={handleChange}
+            input={<OutlinedInput label="Tag" />}
+            renderValue={(selected) => selected.join(", ")}
+            MenuProps={MenuProps}
+          >
+            {props.tags.map((name) => (
+              <MenuItem key={name.name} value={name.name}>
+                <Checkbox checked={tagName.indexOf(name.name) > -1} />
+                <ListItemText primary={name.name} />
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+    )
   );
 }
