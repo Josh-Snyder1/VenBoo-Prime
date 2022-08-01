2213c78ab9cc419f1841b8a35d0dbd28985a239b
Tarek Azzazi <azzazi.tarek@gmail.com>
2022-08-01 10:50:17 -0500

    host can see event details

--- src/components/EventDetails/Header.jsx
+++ src/components/EventDetails/Header.jsx
@@ -4,43 +4,68 @@ import { useState, useEffect } from "react";
 import moment from "moment";
 
 function Header() {
+  let Pending = 0;
+  let Approved = 0;
   const { eventId } = useParams();
   const events = useSelector((store) => store.events);
   const booths = useSelector((store) => store.boothApplications);
   const user = useSelector((store) => store.user);
-  console.log(booths);
 
-  //   if (booths.approved_by_host === true) {
-  //     console.log("WHIPPI ITS TRUE");
-  //   }
-
-  return events.map((event) => {
+  let eventDetails = events.filter((event) => {
     if (event.id === Number(eventId)) {
-      return (
-        <ul>
-          {event.name}
-          <br />
-          {moment(event.start_date).format("MMMM Do")} -{" "}
-          {moment(event.end_date).format("MMMM Do YYYY")}
-          <br />
-        </ul>
-      );
+      return event;
     }
-    return booths.map((booth) => {
-      // checks to see if the booth is approved and If it belongs to the current user
-      // If true then renders the Quantity of approved booths
-      if (booth.approved_by_host === true && user.id === booth.vendor_id) {
-        return (
-          <ul key={booth.booth_id}>
-            <li> Approved booths: {booth.quantity} </li>
-          </ul>
-        );
-      }
-      if (booth.approved_by_host === false) {
-        return <h2>Remaining booths: {booth.quantity}</h2>;
-      }
-    });
   });
+  eventDetails = eventDetails.pop();
+
+  for (const booth of booths) {
+    // Checks to see if the booth is approved and If it belongs to the current user
+    // Renders total Approved and total Pending booths quantity
+    if (user.id === booth.vendor_id) {
+      if (booth.approved_by_host === "PENDING") {
+        Pending += booth.quantity;
+      }
+      if (booth.approved_by_host === "APPROVED") {
+        Approved += booth.quantity;
+      }
+    }
+  }
+  switch (user.type) {
+    case "host":
+      return (
+        <>
+          {eventDetails && (
+            <ul>
+              <h2> {eventDetails.name} </h2>
+              <br />
+              {moment(eventDetails.start_date).format("MMMM Do")} -{" "}
+              {moment(eventDetails.end_date).format("MMMM Do YYYY")}
+              <br />
+              Total Approved Booths: {Approved}
+              <br />
+              Pending Booths: {Pending}
+            </ul>
+          )}
+        </>
+      );
+    case "vendor":
+      return (
+        <>
+          {eventDetails && (
+            <ul>
+              <h2> {eventDetails.name} </h2>
+              <br />
+              {moment(eventDetails.start_date).format("MMMM Do")} -{" "}
+              {moment(eventDetails.end_date).format("MMMM Do YYYY")}
+              <br />
+              Avalible booths:
+            </ul>
+          )}
+        </>
+      );
+    default:
+      return <></>;
+  }
 }
 
 export default Header;
