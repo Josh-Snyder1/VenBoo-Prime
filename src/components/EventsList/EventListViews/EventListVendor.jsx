94ab47698fde9821c86802b3af8aa6e21e866f42
Neil Hanson <pro.neilhanson@gmail.com>
2022-08-02 09:56:23 -0500

    got new event to display on vendor dashboard

--- src/components/EventsList/EventListViews/EventListVendor.jsx
+++ src/components/EventsList/EventListViews/EventListVendor.jsx
@@ -8,13 +8,13 @@ import { useDispatch } from "react-redux";
 function EventListVendor() {
     // Stores
     const vendorBooths = useSelector((store) => store.vendorBoothsReducer);
-
-    // Today's date.
-    const todayDate = moment().format('YYYYMMDD');
+    const allEvents = useSelector((store) => store.events);
 
     // Local state to render items the user wants to view.
     const [viewList, setViewList] = useState('approved');
 
+    
+    const todayDate = moment().format('YYYYMMDD');
     const history = useHistory();
     const dispatch = useDispatch();
 
@@ -23,7 +23,16 @@ function EventListVendor() {
     },[])
 
 
-    return (
+    const newEvents= allEvents.filter(event => {
+        for (let vBooth of vendorBooths) {
+           if (vBooth.event_id === event.id) {
+             return false;
+           }
+        }
+        return true;
+    });
+
+        return (
         <Grid 
                 container
                 direction="column"
@@ -49,6 +58,7 @@ function EventListVendor() {
                     <Button onClick={() => setViewList('approved')}>Approved</Button>
                     <Button onClick={() => setViewList('pending')}>Pending</Button>
                     <Button onClick={() => setViewList('past')}>Past</Button>
+                    <Button onClick={() => setViewList('new_events')}>New Events</Button>
                     <br/>
                 </Stack>
                 <Stack
@@ -59,112 +69,146 @@ function EventListVendor() {
                     sx={{
                         display: 'flex',
                         flexWrap: 'wrap',
-                        marginTop: '1em'
+                        margin: '1em'
                     }}
                 >
-                    {vendorBooths.map((booth) => {
-                        console.log(booth)
+                    {/* New Events viewList option render */}
+                    {newEvents.map(event => {
                         if (
+                            viewList === 'new_events'
+                            &&
+                            Number(moment(event.start_date).format('YYYYMMDD')) > Number(todayDate)
+                        ){
+                            return(
+                                <Card
+                                onClick={() => history.push(`/event/${event.id}`)}
+                                key={event.id}
+                                elevation={4}
+                                sx={{
+                                    padding: "1em",
+                                    margin: '1em'
+                                }}
+                                >
+                                <h2>{event.name}</h2>
+                                <h4>{moment(event.start_date).format('MMM DD YYYY')} - {moment(event.end_date).format('MMM DD YYYY')}</h4>
+                                <h5>{event.description}</h5>
+                                </Card>
+                            )
+                        }
+                    })}
+
+                    {/* Approved viewList option render */}
+                    {vendorBooths.map((booth) => {
+                        if(
                             viewList === 'approved'
                             &&
                             Number(moment(booth.start_date).format('YYYYMMDD')) > Number(todayDate)
                             &&
                             booth.approved_by_host
-                        )
-                        return (
-                            <Card
-                                onClick={() => history.push(`/event/${booth.id}`)}
-                                key={booth.id}
-                                elevation={4}
-                                sx={{
-                                    padding: "1em"
-                                }}
-                            >
-                                <h3>{booth.name}</h3>
-                                <h5>{moment(booth.start_date).format('MMM DD YYYY')} - {moment(booth.end_date).format('MMM DD YYYY')}</h5>
-                                <ul>
-                                    <li>Type: {booth.type}</li>
-                                    <li>Dimensions: {booth.dimensions}</li>
-                                    <li>Quantity: {booth.quantity}</li>
-                                    <li>Description: {booth.description}</li>
-                                    <li>Cost: ${booth.cost}</li>
-                                    <li>Requested on: {moment(booth.requested_on).format('MMM DD YYYY')}</li>
-                                    {booth.approved_by_host ? 
-                                        <li>Application Status: Approved ✅</li>
-                                        :
-                                        <li>Application Status: Pending ❌</li>
-                                    }
-                                </ul>
-                                <br/>
-                            </Card>
-                        )
-                        else if (
+                        ){
+                            return (
+                                <Card
+                                    onClick={() => history.push(`/event/${booth.id}`)}
+                                    key={booth.id}
+                                    elevation={4}
+                                    sx={{
+                                        padding: "1em"
+                                    }}
+                                >
+                                    <h3>{booth.name}</h3>
+                                    <h5>{moment(booth.start_date).format('MMM DD YYYY')} - {moment(booth.end_date).format('MMM DD YYYY')}</h5>
+                                    <ul>
+                                        <li>Type: {booth.type}</li>
+                                        <li>Dimensions: {booth.dimensions}</li>
+                                        <li>Quantity: {booth.quantity}</li>
+                                        <li>Description: {booth.description}</li>
+                                        <li>Cost: ${booth.cost}</li>
+                                        <li>Requested on: {moment(booth.requested_on).format('MMM DD YYYY')}</li>
+                                        {booth.approved_by_host ? 
+                                            <li>Application Status: Approved ✅</li>
+                                            :
+                                            <li>Application Status: Pending ❌</li>
+                                        }
+                                    </ul>
+                                    <br/>
+                                </Card>
+                            )
+                        }
+
+                        {/* Pending viewList option render */}
+                        if (
                             viewList === 'pending'
                             &&
                             Number(moment(booth.start_date).format('YYYYMMDD')) > Number(todayDate)
                             &&
                             !booth.approved_by_host
-                        )
-                        return (
-                            <Card
-                                onClick={() => history.push(`/event/${booth.id}`)}
-                                key={booth.id}
-                                elevation={4}
-                                sx={{
-                                    padding: "1em"
-                                }}
-                            >
-                                <h3>{booth.name}</h3>
-                                <h5>{moment(booth.start_date).format('MMM DD YYYY')} - {moment(booth.end_date).format('MMM DD YYYY')}</h5>
-                                <ul>
-                                    <li>Type: {booth.type}</li>
-                                    <li>Dimensions: {booth.dimensions}</li>
-                                    <li>Quantity: {booth.quantity}</li>
-                                    <li>Description: {booth.description}</li>
-                                    <li>Cost: ${booth.cost}</li>
-                                    <li>Requested on: {moment(booth.requested_on).format('MMM DD YYYY')}</li>
-                                    {booth.approved_by_host ? 
-                                        <li>Application Status: Approved ✅</li>
-                                        :
-                                        <li>Application Status: Pending ❌</li>
-                                    }
-                                    <li>Pending</li>
-                                </ul>
-                                <br/>
-                            </Card>
-                        )
-                        else if (
+                        ){
+                            return (
+                                <Card
+                                    onClick={() => history.push(`/event/${booth.id}`)}
+                                    key={booth.id}
+                                    elevation={4}
+                                    sx={{
+                                        padding: "1em"
+                                    }}
+                                >
+                                    <h3>{booth.name}</h3>
+                                    <h5>{moment(booth.start_date).format('MMM DD YYYY')} - {moment(booth.end_date).format('MMM DD YYYY')}</h5>
+                                    <ul>
+                                        <li>Type: {booth.type}</li>
+                                        <li>Dimensions: {booth.dimensions}</li>
+                                        <li>Quantity: {booth.quantity}</li>
+                                        <li>Description: {booth.description}</li>
+                                        <li>Cost: ${booth.cost}</li>
+                                        <li>Requested on: {moment(booth.requested_on).format('MMM DD YYYY')}</li>
+                                        {booth.approved_by_host ? 
+                                            <li>Application Status: Approved ✅</li>
+                                            :
+                                            <li>Application Status: Pending ❌</li>
+                                        }
+                                        <li>Pending</li>
+                                    </ul>
+                                    <br/>
+                                </Card>
+                            )
+                        }
+
+                        {/* Past viewList option render */}
+                        if (
                             viewList === 'past'
                             &&
                             Number(moment(booth.start_date).format('YYYYMMDD')) < Number(todayDate)
-                        )
-                        return (
-                            <Card
-                                onClick={() => history.push(`/event/${booth.id}`)}
-                                key={booth.id}
-                                elevation={4}
-                                sx={{
-                                    padding: "1em"
-                                }}
-                            >
-                                <h3>{booth.name}</h3>
-                                <h5>{moment(booth.start_date).format('MMM DD YYYY')} - {moment(booth.end_date).format('MMM DD YYYY')}</h5>
-                                <ul>
-                                    <li>Type: {booth.type}</li>
-                                    <li>Dimensions: {booth.dimensions}</li>
-                                    <li>Quantity: {booth.quantity}</li>
-                                    <li>Description: {booth.description}</li>
-                                    <li>Cost: ${booth.cost}</li>
-                                    <li>Requested on: {moment(booth.requested_on).format('MMM DD YYYY')}</li>
-                                    {booth.approved_by_host ? 
-                                        <li>Application Status: Approved ✅</li>
-                                        :
-                                        <li>Application Status: Pending ❌</li>
-                                    }
-                                </ul>
-                                <br/>
-                            </Card>
-                        )
+                            &&
+                            booth.approved_by_host
+                        ){
+                            return (
+                                <Card
+                                    onClick={() => history.push(`/event/${booth.id}`)}
+                                    key={booth.id}
+                                    elevation={4}
+                                    sx={{
+                                        padding: "1em"
+                                    }}
+                                >
+                                    <h3>{booth.name}</h3>
+                                    <h5>{moment(booth.start_date).format('MMM DD YYYY')} - {moment(booth.end_date).format('MMM DD YYYY')}</h5>
+                                    <ul>
+                                        <li>Type: {booth.type}</li>
+                                        <li>Dimensions: {booth.dimensions}</li>
+                                        <li>Quantity: {booth.quantity}</li>
+                                        <li>Description: {booth.description}</li>
+                                        <li>Cost: ${booth.cost}</li>
+                                        <li>Requested on: {moment(booth.requested_on).format('MMM DD YYYY')}</li>
+                                        {booth.approved_by_host ? 
+                                            <li>Application Status: Approved ✅</li>
+                                            :
+                                            <li>Application Status: Pending ❌</li>
+                                        }
+                                    </ul>
+                                    <br/>
+                                </Card>
+                            )
+                        }
                     })}
                 </Stack>
             </Grid>
