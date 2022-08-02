850e6d6ee116f531e31ee2c68930c61d45b18c4a
Nimo Abdi <nimoabdi318@gmail.com>
2022-08-02 15:30:47 -0500

    mui styling

--- src/components/EventDetails/EventDetails.jsx
+++ src/components/EventDetails/EventDetails.jsx
@@ -1,43 +1,48 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory, useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
+import './EventDetails.css'
+// MUI
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+import SendIcon from '@mui/icons-material/Send';
+import Stack from '@mui/material/Stack';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 function EventDetails() {
 
+    function createData(vendor, tags, booth, description ) {
+        return { vendor, tags, booth, description };
+      }
 
     const eventBoothDetails = useSelector((store) => store.boothApplications);
     // const tagsBooth = useSelector((store)=> store.tagsReducer);
     const dispatch = useDispatch();
     const user = useSelector((store) => store.user);
     const history = useHistory();
-    const [viewList, setViewList] = useState('');
+    const [viewList, setViewList] = useState(''); 
 
     // delete booth handle
-    // function deleteBooth(id){
-    //     dispatch({ type: 'DELETE_BOOTH', payload: {id}})
-    //   }
 
     function handleDelete(id) {
         dispatch({
           type: "DELETE_BOOTH",
-          payload: { id }
+          payload: {id: booths.booth_id}
         });
         console.log("delete booth>>>>>>>", id);
       };
+
     
     // edit put booth
 
     const { eventId } = useParams();
-    // console.log(eventId)
 
-    //   deleteProps={
-    //       id: booths.id,
-    //       dispatchName: 'DELETE_BOOTH_APPLICATION'
-    //   }
-
-    // <DeleteButton props={deleteProps} />
-
-    // console.log('event booth', eventBoothDetails)
 
     useEffect(() => {
         dispatch({
@@ -48,10 +53,10 @@ function EventDetails() {
         })
       }, [eventId]);
 
+ 
 
-
-console.log('event booth', eventBoothDetails);
-// console.log('tags event booth', tagsBooth);
+    console.log('event booth', eventBoothDetails);
+    // console.log('tags event booth', tagsBooth);
     
     return (
         // adding booths and available booths
@@ -59,7 +64,7 @@ console.log('event booth', eventBoothDetails);
       <h1>Available Booths</h1>
       <div>
         <button>Add Booth Type</button>
-        <table className='booths info'>
+        <table className='booths_info'>
             
             <thead>
                 <tr>
@@ -89,71 +94,72 @@ console.log('event booth', eventBoothDetails);
         </table>
       </div>
         
-    <div>
-      
-      
-        <h1>Pending Approval</h1>
-        <table className="pending">
-                <thead>
-                    <tr>
-                        <th>Vendor Name</th>
-                        <th>Tags</th>
-                        <th>Booth Size</th>
-                    </tr>
-                </thead>
+    <TableContainer component={Paper} >
+        <h2>Pending Approval</h2>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table" className="pending">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Vendor Name</TableCell>
+                        <TableCell>Tags</TableCell>
+                        <TableCell>Booth Size</TableCell>
+                    </TableRow>
+                </TableHead>
 
-                <tbody>
+                <TableBody>
                     {eventBoothDetails.map((booths)=> {
                         console.log('booth id is>>>>>', booths.booth_id);
                         return(
                             
                         
-                            <tr key={booths.id}> 
-
-                                <td>{booths.business_name}</td>
-                                <td>{booths.tags}</td>
-                                <td>{booths.dimensions}</td>
-                                <button>✅</button>
-                                <button onClick={ () => dispatch({type: 'DELETE_BOOTH', payload: {id: booths.booth_id} })} >❌</button>
-                            </tr>
+                            <TableRow key={booths.id}> 
+                            
+                                <TableCell >{booths.business_name}</TableCell>
+                                <TableCell>{booths.tags}</TableCell>
+                                <TableCell>{booths.dimensions}</TableCell>
+                                <Stack direction="row" spacing={2}></Stack>
+                                <Button size="small" variant="outlined" >Approve</Button>
+                                <Button size="small" variant="outlined" startIcon={<DeleteIcon />} onClick={ () => dispatch({type: 'DELETE_BOOTH', payload: {id: booths.booth_id} })} >Delete</Button>
+                    
+                            </TableRow>
                             )
                         })}
-                        </tbody>
-        </table>
+                        </TableBody>
+        </Table>
                         
-      </div>
+      </TableContainer>
 
-      <div>
-        <h1>Approved</h1>
-        <table className="approved">
-                <thead>
-                    <tr>
-                        <th>Vendor Name</th>
-                        <th>Tags</th>
-                        <th>Booth Size</th>
-                        <th>Description</th>
-                    </tr>
-                </thead>
+      <TableContainer component={Paper}>
+        <h2>Approved</h2>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table" className="approved">
+                <TableHead>
+                    <TableRow>
+                        <TableCell > Vendor Name</TableCell>
+                        <TableCell> Tags</TableCell>
+                        <TableCell> Booth Size</TableCell>
+                        <TableCell> Description</TableCell>
+                    </TableRow>
+                </TableHead>
                 
                 
-                <tbody>
+                <TableBody>
                     {eventBoothDetails.map((list)=> {
                         if (list.approved_by_host &&
                             list.id === user.id &&
                             list.verified
                             )
                         return(
-                            <tr key={list.id}>
-                                <td>{list.business_name}</td>
-                                <td>{list.tags}</td>
-                                <td>{list.dimensions}</td>
-                                <td>{list.description}</td>
-                            </tr>
+                            <TableRow key={list.id}>
+                                <TableCell >{list.business_name}</TableCell >
+                                <TableCell >{list.tags}</TableCell >
+                                <TableCell >{list.dimensions}</TableCell >
+                                <TableCell >{list.description}</TableCell >
+                                <Button variant="outlined" disabled>Approved</Button>
+                            </TableRow>
                         )
                     })}
-                </tbody>
-        </table>
-      </div>
+                </TableBody>
+        </Table>
+      </TableContainer>
         </>
     )
                 
