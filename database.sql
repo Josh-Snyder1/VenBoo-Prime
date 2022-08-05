51ea179a82bbdf9bc89c24202520e574aea20b1d
Colin Oraskovich <Colin.Oraskovich@gmail.com>
2022-08-04 21:15:58 -0500

    Improved database creation and data dump file

--- database.sql
+++ database.sql
@@ -70,10 +70,12 @@ CREATE TABLE "user" (
 	"phone" VARCHAR (15),
 	"phone_extension" VARCHAR(15),
 	"approved_host" BOOLEAN NOT NULL DEFAULT FALSE,
-	"main_url" VARCHAR (255),
-	"facebook_url" VARCHAR (255),
-	"etsy_url" VARCHAR (255),
-	"linkedin_url" VARCHAR (255),
+	"main_url" VARCHAR (1023),
+	"facebook_url" VARCHAR (1023),
+	"etsy_url" VARCHAR (1023),
+	"linkedin_url" VARCHAR (1023),
+	"instagram_url" VARCHAR (1023),
+	"pinterest_url" VARCHAR (1023),
 	"created_on" TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
 	"last_login" TIMESTAMP WITH TIME ZONE
 );
@@ -140,49 +142,3 @@ CREATE TABLE "event_tags" (
 	"tag_id" INTEGER REFERENCES "tags" ON DELETE CASCADE,
 	UNIQUE ("event_id", "tag_id")
 );
-
-
--- Below is to insert dummy/starting data into tables
-INSERT INTO "tags" (name)
-VALUES ('Showcase'),('Antiques'),('Food Vendor'),('Marketer'),('Content Creator'),('Craft Maker');
-
-INSERT INTO "addresses" ("address", "address_2", "city", "state", "zipcode")
-VALUES ('123 Street Ave', '321 Drive Way', 'Saint Paul', 'MN', 55415);
-
-INSERT INTO "user" ("email", "password", "type", "first_name", "last_name", "title", "business_name", "description","address_id", "phone","main_url", "facebook_url", "etsy_url", "linkedin_url")
-VALUES
-('admin1@gmail.com', '$2a$10$1luP05zl8k51vCWkTaSu9.O8jwisNGB3dhzwoALeJynhbyNTj0QKe', 'admin', 'Colin', 'Lastname', 'CEO', 'Weather', 'Weather is a cool website', '1', '123-123-1234', 'https://github.com/PrimeAcademy', 'https://www.facebook.com/', 'https://www.etsy.com/', 'https://www.linkedin.com'),
-('vendor1@gmail.com', '$2a$10$1luP05zl8k51vCWkTaSu9.O8jwisNGB3dhzwoALeJynhbyNTj0QKe', 'vendor', 'Neil', 'Lastname', 'CEO', 'Banana Store', 'We only sell the finest bananas', '1', '123-123-1234', 'https://github.com/PrimeAcademy', 'https://www.facebook.com/', 'https://www.etsy.com/', 'https://www.linkedin.com'),
-('vendor2@gmail.com', '$2a$10$1luP05zl8k51vCWkTaSu9.O8jwisNGB3dhzwoALeJynhbyNTj0QKe', 'vendor', 'Nimo', 'Lastname', 'CEO', 'TV Shows Store', 'We sell tv shows', '1', '123-123-1234', 'https://github.com/PrimeAcademy', 'https://www.facebook.com/', 'https://www.etsy.com/', 'https://www.linkedin.com'),
-('vendor3@gmail.com', '$2a$10$1luP05zl8k51vCWkTaSu9.O8jwisNGB3dhzwoALeJynhbyNTj0QKe', 'vendor', 'John', 'Lastname', 'CEO', 'Dog Store', 'We make good daog treats', '1', '123-123-1234', 'https://github.com/PrimeAcademy', 'https://www.facebook.com/', 'https://www.etsy.com/', 'https://www.linkedin.com'),
-('vendor4@gmail.com', '$2a$10$1luP05zl8k51vCWkTaSu9.O8jwisNGB3dhzwoALeJynhbyNTj0QKe', 'vendor', 'Edan', 'Lastname', 'CEO', 'Mushroom Store', 'We sell great shrooms', '1', '123-123-1234', 'https://github.com/PrimeAcademy', 'https://www.facebook.com/', 'https://www.etsy.com/', 'https://www.linkedin.com'),
-('host1@gmail.com', '$2a$10$1luP05zl8k51vCWkTaSu9.O8jwisNGB3dhzwoALeJynhbyNTj0QKe', 'host', 'Tarek', 'Lastname', 'CEO', 'Food Host Company', 'Food Event is a cool website', '1', '123-123-1234', 'https://github.com/PrimeAcademy', 'https://www.facebook.com/', 'https://www.etsy.com/', 'https://www.linkedin.com'),
-('host2@gmail.com', '$2a$10$1luP05zl8k51vCWkTaSu9.O8jwisNGB3dhzwoALeJynhbyNTj0QKe', 'host', 'Josh', 'Lastname', 'CEO', 'River Run Hosting Company', 'River Run Event is a cool website', '1', '123-123-1234', 'https://github.com/PrimeAcademy', 'https://www.facebook.com/', 'https://www.etsy.com/', 'https://www.linkedin.com'),
-('host3@gmail.com', '$2a$10$1luP05zl8k51vCWkTaSu9.O8jwisNGB3dhzwoALeJynhbyNTj0QKe', 'host', 'Jean', 'Lastname', 'CEO', 'Wine Tasting Events INC', 'WE host the biggest wine tasting events in MN!', '1', '123-123-1234', 'https://github.com/PrimeAcademy', 'https://www.facebook.com/', 'https://www.etsy.com/', 'https://www.linkedin.com'),
-('host4@gmail.com', '$2a$10$1luP05zl8k51vCWkTaSu9.O8jwisNGB3dhzwoALeJynhbyNTj0QKe', 'host', 'Alex', 'Lastname', 'CEO', 'Hotdog Eating Events INC', 'We only host the greatest hotdog events', '1', '123-123-1234', 'https://github.com/PrimeAcademy', 'https://www.facebook.com/', 'https://www.etsy.com/', 'https://www.linkedin.com');
-
-
-INSERT INTO "venues" ("name", "address_id", "contact_name", "contact_phone", "contact_email", "contact_url")
-VALUES
-('Xcel Energy Center', 1, 'Bill Billson', '123-123-1234', 'test@gmail.com', 'https://www.xcelenergycenter.com/');
-
-INSERT INTO "events" (user_id, name, venue_id, description, start_date, end_date)
-VALUES
-(4, 'Billys Bee Bonanza', 1, 'And event for bee keepers and lovers to explore the world of bee keeping', '2022-07-16', '2022-07-17'),
-(4, 'Minnesota State Fair', 1, 'The Minnesota State Fair is one of the largest and best-attended expositions in North America, attracting 2 million visitors annually. In addition to being a showcase for Minnesota’s finest agriculture, art and industry, the fair features hundreds of entertainment options including music all around the fairgrounds; educational exhibits; hands-on experiences; more than 60 carnival rides; thousands of competitions; 11 nights of Grandstand shows; and more than 500 different foods.', '2022-08-25', '2022-09-5'),
-(5, 'Farm Fest', 1, 'At Farmfest, we strive to bring together the best in agribusiness from Minnesota and around the country. We endeavor to provide one place for farmers to network, experience and learn to grow their farming operations.', '2022-08-06', '2022-08-07'),
-(5, 'Comic Con', 1, 'A comic book convention or comic con is an event with a primary focus on comic books and comic book culture, in which comic book fans gather to meet creators, experts, and each other. Commonly, comic conventions are multi-day events hosted at convention centers, hotels, or college campuses.', '2022-09-06', '2022-09-07');
-
-INSERT INTO "booths" (event_id, type, dimensions, quantity, description, cost)
-VALUES
-(1, 'small', '5x5', 1, '1 table and 2 chairs', 200), (2, 'premium', '20x25', 2, '1 tent, 4 tables, and 8 chairs', 1200),
-(3, 'large', '18x18', 4, 'mega tables', 2000), (4, 'meduim', '6x6', 3, 'nothing', 400);
-
-INSERT INTO "booth_applications" (booth_id, user_id, approved_by_host)
-VALUES
-(1, 2, 'PENDING'), (2, 3, 'APPROVED'), (3, 3, 'APPROVED'), (4, 2, 'REJECTED');
-
-INSERT INTO "vendor_tags" ("user_id", "tag_id")
-VALUES
-(1,1), (2,2), (2,3), (3,3), (4,4), (5,3), (6,2);
-
