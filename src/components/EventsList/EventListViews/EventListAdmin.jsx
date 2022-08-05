c50ffb36ffcd712b645698919429a6dbc8836d01
Colin Oraskovich <Colin.Oraskovich@gmail.com>
2022-08-05 10:01:23 -0500

    Fixed the dataDump file to point events to the corresponding host. Also, updated the admin event display list

--- src/components/EventsList/EventListViews/EventListAdmin.jsx
+++ src/components/EventsList/EventListViews/EventListAdmin.jsx
@@ -10,6 +10,9 @@ import { Grid, Stack, Tabs, Tab } from "@mui/material";
 import EventCardComponent from "./EventListComponents/EventCardComponent";
 import VendorCardComponent from "./EventListComponents/VendorCardComponent";
 
+// Import used functions
+import { AddDayToDate } from "../../Utilities/SetDateRangeFromDate";
+
 
 // Component that shows all the events that the admin can see
 export default function EventListAdmin() {
@@ -21,7 +24,7 @@ export default function EventListAdmin() {
   const allEvents = useSelector((store) => store.events);
 
   // Local state to render items the user wants to view.
-  const [viewList, setViewList] = useState('approved');
+  const [viewList, setViewList] = useState('APPROVED');
 
   // Initialize the dispatch functions
   const dispatch = useDispatch();
@@ -35,6 +38,23 @@ export default function EventListAdmin() {
   // displaying the list of events
   const handleChange = (event, newValue) => {
     setViewList(newValue);
+  }
+
+
+  // Filter the events list down by the currently
+  // selected label
+  const filterEventByCategory = (allEvents) => {
+
+    switch (viewList) {
+      case "APPROVED":
+        return allEvents.filter(eventObj => eventObj.verified)
+      case "PENDING":
+        return allEvents.filter(eventObj => !eventObj.verified)
+      case "PAST":
+        return allEvents.filter(eventObj => AddDayToDate(eventObj.end_date) < new Date())
+      default:
+        return allEvents
+    }
   }
 
 
@@ -54,10 +74,10 @@ export default function EventListAdmin() {
       <h3>Events List</h3>
       <br/>
       <Tabs value={viewList} onChange={handleChange}>
-        <Tab value="approved" label="Approved"/>
-        <Tab value="pending" label="Pending"/>
-        <Tab value="past" label="Past"/>
-        <Tab value="new_events" label="New Events"/>
+        <Tab value="APPROVED" label="Approved"/>
+        <Tab value="PENDING" label="Pending"/>
+        <Tab value="PAST" label="Past"/>
+        <Tab value="NEW_EVENTS" label="New Events"/>
       </Tabs>
       <Stack
         direction="row"
@@ -70,60 +90,16 @@ export default function EventListAdmin() {
           margin: '1em'
         }}
       >
-        {/* New Events viewList option render */}
-        {allEvents.map(event => {
-          if (
-            viewList === 'new_events'
-            &&
-            Number(moment(event.end_date).format('YYYYMMDD')) > Number(todayDate)
-          ){
-            return(
-              <EventCardComponent event={event} />
-            )
-          }
+        {/* Filters the events list by the user selected category up top */}
+        {filterEventByCategory(allEvents).map(event => {
+
+          // Build the event list blocks
+          return(
+            <EventCardComponent event={event} />
+          )
         })}
-
-            {/* Approved viewList option render */}
-            {/* {vendorBooths.map((booth) => {
-              if(
-                viewList === 'approved'
-                &&
-                Number(moment(booth.start_date).format('YYYYMMDD')) > Number(todayDate)
-                &&
-                booth.approved_by_host
-              ){
-                return (
-                  <VendorCardComponent booth={booth} />
-                )
-              } */}
-
-              {/* Pending viewList option render */}
-              {/* if (
-                viewList === 'pending'
-                &&
-                Number(moment(booth.start_date).format('YYYYMMDD')) > Number(todayDate)
-                &&
-                !booth.approved_by_host
-              ){
-                return (
-                  <VendorCardComponent booth={booth} />
-                )
-              } */}
-
-              {/* Past viewList option render */}
-              {/* if (
-                viewList === 'past'
-                &&
-                Number(moment(booth.start_date).format('YYYYMMDD')) < Number(todayDate)
-                &&
-                booth.approved_by_host
-              ){
-                return (
-                  <VendorCardComponent booth={booth} />
-                )
-              }
-            })} */}
-        </Stack>
+        
+      </Stack>
     </Grid>
   )
 }
