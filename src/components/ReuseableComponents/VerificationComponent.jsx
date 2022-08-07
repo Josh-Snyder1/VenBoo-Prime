48f31244d390fd57bf07f61de3137a6dce7f8c0f
Josh Snyder <joshrsnyder@gmail.com>
2022-08-07 16:07:27 -0500

    still working on verification component

--- src/components/ReuseableComponents/VerificationComponent.jsx
+++ src/components/ReuseableComponents/VerificationComponent.jsx
@@ -2,12 +2,19 @@ import * as React from 'react';
 import Alert from '@mui/material/Alert';
 import Stack from '@mui/material/Stack';
 import Button from "@mui/material/Button";
-import { useSelector } from "react-redux";
+import Chip from '@mui/material/Chip';
+import StarIcon from '@mui/icons-material/Star';
+import { useDispatch, useSelector } from "react-redux";
 
 function VerificationComponent({props}) {
 
+    const dispatch = useDispatch();
+
+    const user = useSelector((store) => store.user);
+
     // event details of this specific event
-    const event = props.eventDetails;
+    const event = props?.eventDetails;
+    const profileInfo = props?.profileInfo;
     // render of 
     const renderType = props.type;
 
@@ -17,19 +24,26 @@ function VerificationComponent({props}) {
     console.log('type/view', renderType, view)
 
     function handleVerify() {
-        console.log('handleVerify')
+        console.log('handleVerify', event.id)
+        
     }
-
-    const user = useSelector((store) => store.user);
 
     return (
         // if not admin, only show verification star
-    user &&
+    user?.type === 'vendor' || 'host' && event?.verified === true ? 
+        <>
+            <Stack sx={{display:'flex', flexDirection: 'row-reverse'}} direction="row" spacing={1}>
+                <Alert variant="filled" severity="success" sx={{margin: 2}}>
+                Verified!
+                </Alert>
+            </Stack>
+        </>
+    :
         <Stack sx={{ width: 600, padding: 3, borderRadius:5 }} spacing={2}>
         <Alert severity="warning" >
             This Event is not Verified! - Do you wish to verify it?
             <Button sx={{marginLeft: 10}}
-            onClick={() => {handleVerify}}>
+            onClick={() => {handleVerify()}}>
                 VERIFY
             </Button>
         </Alert>
