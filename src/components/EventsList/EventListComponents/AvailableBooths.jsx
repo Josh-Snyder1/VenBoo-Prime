f3814df2fdf18ef7794e8380603a891dd20cd5e3
Colin Oraskovich <Colin.Oraskovich@gmail.com>
2022-08-09 08:00:57 -0500

    Styled the admin event list

--- src/components/EventsList/EventListComponents/AvailableBooths.jsx
+++ src/components/EventsList/EventListComponents/AvailableBooths.jsx
@@ -1,13 +1,11 @@
 // Component that determines the remaining booth display
 export default function AvailableBooths({ event }) {
 
-  console.log("))))))))", event)
-
   // Check if the event is over-booked
   if (event.reserved_booths > event.total_booths) {
     return (
       <>
-        <div>
+        <div className="available-booths">
           <p className="booths-overbooked">Over Booked</p>
           <p><span>{event.reserved_booths}</span> of <span>{event.total_booths}</span></p>
         </div>
@@ -19,7 +17,7 @@ export default function AvailableBooths({ event }) {
   if (event.reserved_booths === event.total_booths) {
     return (
       <>
-        <div>
+        <div className="available-booths">
           <p className="booths-event-full">Event Full</p>
           <p><span>{event.reserved_booths}</span> of <span>{event.total_booths}</span></p>
         </div>
@@ -30,7 +28,7 @@ export default function AvailableBooths({ event }) {
   // Default display for the available booths
   return (
     <>
-      <div>
+      <div className="available-booths">
         <p><span>{event.reserved_booths}</span> of <span>{event.total_booths}</span> booths reserved</p>
         <p><span>{event.available_booths}</span> still available</p>
       </div>
